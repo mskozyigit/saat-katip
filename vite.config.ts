964af326bf1b02d72eaf01ac770df902afde15ad
mskozyigit <mskozyigit@users.noreply.github.com>
2026-07-18 14:05:21 +0200

    Fix: base path / olarak degistirildi

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   // GitHub Pages'te yayınlanırken base path: '/saat-katip/'
   // Lokal geliştirme için '/' olarak değiştirilebilir
-  base: process.env.NODE_ENV === 'production' ? '/saat-katip/' : '/',
+  base: '/',
   build: {
     outDir: 'dist',
     sourcemap: false,
